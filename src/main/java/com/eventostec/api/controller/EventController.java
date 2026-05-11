34920721e5027160a42e1f6370c366eb0fe8d25f
CauaSean <carreiracaua1@gmail.com>
2026-05-11 16:53:07 -0300

    subindo app na AWS

--- src/main/java/com/eventostec/api/controller/EventController.java
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -1,6 +1,7 @@
 package com.eventostec.api.controller;
 
 import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.domain.event.EventDetailsDTO;
 import com.eventostec.api.domain.event.EventRequestDTO;
 import com.eventostec.api.domain.event.EventResponseDTO;
 import com.eventostec.api.service.EventService;
@@ -12,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/event")
@@ -33,6 +35,13 @@ public class EventController {
         Event newEvent = this.eventService.createEvent(eventRequestDTO);
         return ResponseEntity.ok(newEvent);
     }
+
+    @GetMapping("/{eventId}")
+    public ResponseEntity<EventDetailsDTO> getEventDetails(@PathVariable UUID eventId){
+       EventDetailsDTO eventDetailsDTO = eventService.getEventDetails(eventId);
+       return ResponseEntity.ok(eventDetailsDTO);
+    }
+
 
     @GetMapping
     public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
